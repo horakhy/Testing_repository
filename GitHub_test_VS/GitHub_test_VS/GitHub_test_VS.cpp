c29949eeb26f7104e6f16fc42ad17a8c266a8332
christian antunes <leonidas51000@hotmail.com>
2020-06-10 16:51:36 -0300

    First commit

---
+++ GitHub_test_VS/GitHub_test_VS/GitHub_test_VS.cpp
@@ -0,0 +1,11 @@
+// GitHub_test_VS.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "This is a test" << endl;
+
+	system("pause");
+}
